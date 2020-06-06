baa8e4d6dd5a7135e2e7c647f218815677d692c0
sosno <msosnowski@wsb.gda.pl>
2020-06-06 19:59:12 +0200

    add Seller class

---
+++ src/pl/gda/wsb/employee/Seller.java
@@ -0,0 +1,13 @@
+package pl.gda.wsb.employee;
+
+public class Seller extends Employee {
+
+    public Seller(boolean logged, String name) {
+        super(logged, name);
+    }
+
+    @Override
+    public String getPosition() {
+        return "handlowiec";
+    }
+}
