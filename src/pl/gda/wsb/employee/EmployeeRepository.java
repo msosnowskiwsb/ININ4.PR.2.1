50f371548752ef1d94069e5c20e0e1f86621e3d6
sosno <msosnowski@wsb.gda.pl>
2020-06-06 20:41:27 +0200

    add searchEmployee method

--- src/pl/gda/wsb/employee/EmployeeRepository.java
+++ src/pl/gda/wsb/employee/EmployeeRepository.java
@@ -13,37 +13,63 @@ public class EmployeeRepository {
         Scanner inScanner = new Scanner(System.in);
 
         while (inScanner.hasNextLine()){
-            String employeeNameFromUser = inScanner.nextLine();
 
-            if(employeeNameFromUser.equals("exit")){
-                dataBase.saveToFile(employeeList);
-                break;
+            try {
+                Employee foundEmployee = searchEmployee(employeeList,inScanner.nextLine());
+                foundEmployee.setLogged(!foundEmployee.isLogged());
+                System.out.println("Zmieniono status dla pracownika " + foundEmployee.getName() + " na " + foundEmployee.isLogged() + ".\n\nPodaj imię i nazwisko (exit = koniec):");
+            } catch (WrongEmployee wrongEmployee) {
+                wrongEmployee.printStackTrace();
             }
 
-            int i = 0;
-            boolean searched = false;
-            Pattern patternSearch = Pattern.compile("^(true|false) - " + employeeNameFromUser + " - (.+)$");
 
-            for (Employee employee : getEmployees()){
+            //   String employeeNameFromUser = inScanner.nextLine();
+
+/*            if(employeeNameFromUser.equals("exit")){
+                dataBase.saveToFile(employeeList);
+                break;
+            }*/
+
+   /*         int i = 0;
+            boolean searched = false;*/
+            //Pattern patternSearch = Pattern.compile("^(true|false) - " + employeeNameFromUser + " - (.+)$");
+
+/*            for (Employee employee : getEmployees()){
                 Matcher matcher = patternSearch.matcher(employee.toString());
                 if (matcher.matches()){
                     searched = true;
                     boolean isLogged = Boolean.parseBoolean(matcher.group(1));
                     employeeList.get(i).setLogged(!isLogged);
-                    //getEmployees().remove(i);
-                    //getEmployees().add(i,employee.replace(matcher.group(1), isLogged ? "false" : "true"));
                     break;
                 }
                 i++;
-            }
+            }*/
 
-            if (searched){
+  /*          if (searched){
                 System.out.println("Został zmieniony status dla pracownika: " + employeeNameFromUser);
             } else {
                 System.out.println("Błędnie podane imię i nazwisko!");
-            }
+            }*/
 
         }
+    }
+
+    private Employee searchEmployee(ArrayList<Employee> employeeList, String employeeNameFromUser) throws WrongEmployee {
+        if(employeeNameFromUser.equals("exit")){
+            dataBase.saveToFile(employeeList);
+            System.exit(0);
+        }
+
+        Pattern patternSearch = Pattern.compile("^(true|false) - " + employeeNameFromUser + " - (.+)$");
+
+        for (Employee employee : getEmployees()){
+            Matcher matcher = patternSearch.matcher(employee.toString());
+            if (matcher.matches()){
+                return employee;
+            }
+        }
+
+        throw new WrongEmployee();
     }
 
     ArrayList<Employee> getEmployees(Boolean onlyLooged){
